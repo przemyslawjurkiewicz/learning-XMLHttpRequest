d6d81f72b83745bd55b25afd02cda452747844da
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-23 08:27:16 +0100

    Add script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-'use strict';
-(function () {
-   
-    
+"use strict";
+(function() {
+  var url = "http://api.icndb.com/jokes/random";
+  var paragraph = document.getElementById("joke");
+
+  var button = document.getElementById("get-joke");
+  button.addEventListener("click", function() {
+    getJoke();
+  });
+
+  function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function(){
+      var response = JSON.parse(xhr.response);
+      paragraph.innerHTML = response.value.joke;
+    });
+    xhr.send();
+  }
+  
 })();
