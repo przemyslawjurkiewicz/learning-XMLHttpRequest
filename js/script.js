af4fdcbf7baa23c5e482924b7aa0d9c29f1d0f32
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-23 09:08:41 +0100

    Add onLoad

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 "use strict";
-(function() {
-  var url = "https://api.icndb.com/jokes/random";
-  var paragraph = document.getElementById("joke");
+(function () {
+    var url = "https://api.icndb.com/jokes/random";
+    var paragraph = document.getElementById("joke");
 
-  var button = document.getElementById("get-joke");
-  button.addEventListener("click", function() {
-    getJoke();
-  });
+    document.addEventListener('DOMContentLoaded', getJoke());
 
-  function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-      var response = JSON.parse(xhr.response);
-      paragraph.innerHTML = response.value.joke;
+    var button = document.getElementById("get-joke");
+    button.addEventListener("click", function () {
+        getJoke();
     });
-    xhr.send();
-  }
+
+    function getJoke() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', function () {
+            var response = JSON.parse(xhr.response);
+            paragraph.innerHTML = response.value.joke;
+        });
+        xhr.send();
+    }
 
 })();
